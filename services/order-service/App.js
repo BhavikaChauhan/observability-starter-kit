75c717965cbced29a26bc560346ed15be5d46255
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-07-16 00:23:44 +0530

    Updated App.js

--- services/order-service/App.js
+++ services/order-service/App.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const port = 8000;
 
 app.get('/health', (req, res) => {
   res.send('Order Service is healthy!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Order Service running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Order Service running at http://localhost:${port}`);
 });
